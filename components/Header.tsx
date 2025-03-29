356398a34c74b6e4eb55e7f7a8ce2546d1517cb1
Pi4a <piralith@outlook.fr>
2025-03-29 23:01:14 +0100

    logo color prototype

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import ThemeToggle from "./theme-toggle"
 import Logo from "./logo"
 export default function Header() {
   return (
-    <header className="flex items-center justify-between py-4 px-8	">
+    <header className="flex items-center justify-between py-4 px-8 bg-red-500 dark:bg-black bg-opacity-20">
       <Logo text="DEDU Stéphane" />
       <ThemeToggle />
     </header>
