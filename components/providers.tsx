8d858e3c5741aaeb607bdefbddf0bc216d501795
Pi4a <piralith@outlook.fr>
2025-03-29 19:09:52 +0100

    theme toggle button

---
+++ components/providers.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { ThemeProvider, useTheme } from "next-themes"
+export default function providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  )
+}
