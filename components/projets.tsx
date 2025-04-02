d2a99aa74d83b0a472e787853fc4dbf9bb272bf2
Pi4a <piralith@outlook.fr>
2025-04-02 11:27:38 +0200

    cards and responsive, now modal implementing

--- components/projets.tsx
+++ components/projets.tsx
@@ -1,9 +1,9 @@
 "use client"
 import { useLanguage } from "../contexts/LanguageContext"
 import { translations } from "../translations"
-import ProjectCard from "./projectCard"
 import projetsdata from "../datas/projetsdatanew.json"
-
+import { TiltShineCard } from "./projectCardTilt"
+import Backdrop from "./backdrop"
 interface Projet {
   projectName: string
   projectDescription: string
@@ -30,25 +30,33 @@ function Projets({
         id="projects"
         className="w-full h-[2px] bg-gradient-to-r from-transparent via-pink-300/60 to-transparent "
       />
-      <div className="flex flex-col items-center min-h-[50dvh]">
-        <h1 className="text-4xl font-light tracking-tight text-gray-800 dark:text-gray-200 pt-10 mx-auto">
-          {translations[language].projects.title}
-        </h1>
+      <div className="flex flex-col items-center min-h-[85dvh]">
+        {scrollYValue > 1300 && (
+          <>
+            <h1 className="text-6xl font-light tracking-tight text-gray-800 dark:text-gray-200 pt-10 mx-auto">
+              {translations[language].projects.title}
+            </h1>
 
-        <div className="xl:grid space-y-8 xl:space-y-0  xl:mx-0 xl:grid-cols-3 min-h-[60dvh] gap-10 w-[90%] xl:w-full items-center pt-10 ">
-          {projetsdata.map((projet: Projet, index: number) => (
-            <ProjectCard className="" key={index}>
-              <h3 className="text-2xl flex justify-center xl:py-2 xl:pt-0  font-light tracking-tight text-gray-800  dark:text-gray-200">
-                {projet.projectName}
-              </h3>
-              <img
-                src={projet.projectImg}
-                alt={projet.projectName}
-                className="w-full h-auto rounded-lg shadow-lg object-cover opacity-80"
-              />
-            </ProjectCard>
-          ))}
-        </div>
+            <div className=" flex flex-col xl:grid space-y-8 xl:space-y-0  xl:mx-0 xl:grid-cols-3 min-h-[60dvh] gap-10 w-[90%] xl:w-full items-center pt-10 ">
+              {projetsdata.map((projet: Projet, index: number) => (
+                <TiltShineCard className="" key={index}>
+                  <h3 className="text-2xl flex justify-center pb-2 xl:py-2 xl:pt-0  font-light tracking-tight text-black dark:text-gray-200">
+                    {projet.projectName}
+                  </h3>
+                  <img
+                    src={projet.projectImg}
+                    alt={projet.projectName}
+                    className="w-full h-auto rounded-lg shadow-lg object-cover opacity-80 group-hover:scale-[102%] transition-all duration-500"
+                  />
+                </TiltShineCard>
+              ))}
+            </div>
+
+            {false && (
+              <Backdrop onClick={() => console.log("clicked")}>v</Backdrop>
+            )}
+          </>
+        )}
       </div>
     </>
   )
